9f964e8d98ec837d414de519a440db1dc3afcfae
Joshlanuevo <lanuevo68@gmail.com>
2023-09-23 23:09:15 +0800

    improve-ui-design

--- todo_drf/settings.py
+++ todo_drf/settings.py
@@ -60,7 +60,7 @@ TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
         "DIRS": [
-            os.path.join(BASE_DIR, 'frontend/build')
+            os.path.join(BASE_DIR, 'build')
         ],
         "APP_DIRS": True,
         "OPTIONS": {
@@ -125,7 +125,7 @@ USE_TZ = True
 STATIC_URL = "static/"
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'frontend/build/static')
+    os.path.join(BASE_DIR, 'build/static')
 ]
 
 # Default primary key field type
